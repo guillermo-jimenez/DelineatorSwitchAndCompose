4a84e84b2c0424d9ee5330c08f5f3d730ee9c9df
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2020-08-10 12:18:10 +0200

    Implemented Atrial Fibrillation + Fixed Mixup

--- src/data.py
+++ src/data.py
@@ -58,7 +58,7 @@ class Dataset(torch.utils.data.Dataset):
                  proba_no_ST = 0.15, proba_same_morph = 0.2,
                  proba_elevation = 0.2, proba_interpolation = 0.2,
                  proba_mixup = 0.2, mixup_alpha = 1.0, mixup_beta = 1.0,
-                 proba_TV = 0.05, add_baseline_wander = True, 
+                 proba_TV = 0.05, proba_AF = 0.05, add_baseline_wander = True, 
                  amplitude_std = 0.25, interp_std = 0.25,
                  window = 51, labels_as_masks = True):
         # Segments
@@ -99,6 +99,7 @@ class Dataset(torch.utils.data.Dataset):
         self.proba_no_PQ = proba_no_PQ
         self.proba_no_ST = proba_no_ST
         self.proba_TV = proba_TV
+        self.proba_AF = proba_AF
         self.proba_same_morph = proba_same_morph
         self.proba_elevation = proba_elevation
         self.proba_interpolation = proba_interpolation
@@ -125,6 +126,7 @@ class Dataset(torch.utils.data.Dataset):
         does_not_have_PQ = (np.random.rand(1) > (1-self.proba_no_PQ))
         does_not_have_ST = (np.random.rand(1) > (1-self.proba_no_ST))
         has_TV = (np.random.rand(1) > (1-self.proba_TV))
+        has_AF = (np.random.rand(1) > (1-self.proba_AF))
         has_same_morph = (np.random.rand(1) > (1-self.proba_same_morph))
         has_elevation = (np.random.rand(self.N) > (1-self.proba_elevation))
         has_interpolation = (np.random.rand(self.N) > (1-self.proba_interpolation))
@@ -146,6 +148,13 @@ class Dataset(torch.utils.data.Dataset):
             id_T = np.random.randint(0,len(self.T),size=self.N)
             id_TP = np.random.randint(0,len(self.TP),size=self.N)
 
+        if has_AF:
+            id_P = np.repeat(np.random.randint(0,len(self.P),size=1),self.N)
+            pAF = self.P[self.Pkeys[id_P[0]]]
+            # pAF = interp1d(np.linspace(0,1,pAF.size),pAF)(np.linspace(0,1,pAF.size//2))
+            pdist = 4*self.Pamplitudes.rvs(1)
+            # print(np.unique(id_P))
+            
         # In case QRS is not expressed
         filt_QRS = np.random.rand(self.N) > (1-self.proba_no_QRS)
 
@@ -180,42 +189,72 @@ class Dataset(torch.utils.data.Dataset):
                 
                 # amplitude calculation
                 amplitude = distribution.rvs(1)
-                
+
                 # segment retrieval
+                segment = waves[keys[id]]
+
+                # apply mixup
+                if has_mixup[len(beats)]:
+                    rand_id  = np.random.randint(0,len(keys))
+                    segment2 = waves[keys[rand_id]]
+                    if segment.size != segment2.size:
+                        intlen   = np.random.randint(min([segment.size,segment2.size]),max([segment.size,segment2.size]))
+                        segment  = interp1d(np.linspace(0,1,segment.size),segment)(np.linspace(0,1,intlen))
+                        segment2 = interp1d(np.linspace(0,1,segment2.size),segment2)(np.linspace(0,1,intlen))
+                    (segment,_) = mixup(segment,segment2,self.mixup_alpha,self.mixup_beta)
+                    segment /= np.max(segment)-np.min(segment)+self.eps
+
+                # Apply amplitude modulation
+                segment *= amplitude
+                
+                # Apply TV to ST segment
                 if (j == 3) and has_TV:
                     nx = np.random.randint(32)
                     if nx < 2: continue # Avoid always having a TV with some space between QRS and T
                     segment = np.convolve(np.cumsum(norm.rvs(scale=0.01**(2*0.5),size=nx)),np.hamming(nx)/(nx//2),mode='same')
-                else:
-                    segment = amplitude*waves[keys[id]]
+                # Apply AF
+                # Apply AF
+                if has_AF:
+                    if j == 0:
+                        # print("P WAVE")
+                        segment  = waves[keys[id]]
+                        # print(segment.max()-segment.min())
+                        segment *= pdist/2
+                        # print(pdist)
+                        # print(segment.max()-segment.min())
+                        # print("")
+                    if j == 5:
+                        # print("TP SEGMENT")
+                        i = np.random.choice([0,1])
+                        segment  = waves[keys[id]]
+                        # print(segment.max()-segment.min())
+                        segment  = np.convolve(segment,np.concatenate([((-1)**(i))*pAF,((-1)**(i+1))*pAF]))
+                        segment /= np.max(segment)-np.min(segment)+self.eps
+                        segment *= pdist
+                        # print(pdist)
+                        # print(segment.max()-segment.min())
+                        # print("")
+                        # print("###########")
+                        # print("")
 
-                    # apply mixup
-                    if has_mixup[len(beats)]:
-                        segment2 = waves[keys[np.random.randint(0,len(keys))]]
-                        if segment.size != segment2.size:
-                            intlen = np.random.randint(min([segment.size,segment2.size]),max([segment.size,segment2.size]))
-                            segment = interp1d(np.linspace(0,1,segment.size),segment)(np.linspace(0,1,intlen))
-                            segment2 = interp1d(np.linspace(0,1,segment2.size),segment2)(np.linspace(0,1,intlen))
-                        (segment,_) = mixup(segment,segment2,self.mixup_alpha,self.mixup_beta)
-                        segment /= (np.max(segment)-np.min(segment) + self.eps)
-                
                 # amplitude noising
-                segment *= 0.15*np.random.randn(1)+1 
+                noise = 0.15*np.random.randn(1)+1
+                segment *= noise
                 
                 # single-segment interpolation
                 if has_interpolation[len(beats)]:
                     x = np.linspace(0,1,segment.size)
-                    x_new = np.linspace(0,1,int((segment.size*norm.rvs(1,0.25).clip(min=0.5)).clip(1)))
+                    x_new = np.linspace(0,1,int((segment.size*norm.rvs(1,0.25).clip(min=0.5))))
                     segment = interp1d(x,segment)(x_new)
                 
                 # right extrema elevation/depression
                 if has_elevation[len(beats)]:
-                    right_amplitude = distribution.rvs(1)*0.15
-                    segment += np.sign(right_amplitude)*(np.linspace(0,np.sqrt(np.abs(right_amplitude)),segment.size)**2).squeeze()
+                    right_amplitude = distribution.rvs(1)*0.05
+                    segment += np.random.choice([-1,1])*(np.linspace(0,np.sqrt(np.abs(right_amplitude)),segment.size)**2).squeeze()
                 
                 # onset trailing
                 segment = trailonset(segment,beats[-1][-1] if len(beats) != 0 else 0)
-                
+
                 # final segment storage
                 mask_value = 1 if j==0 else 2 if j==2 else 3 if j==4 else 0
                 beats.append(segment)
@@ -253,4 +292,4 @@ class Dataset(torch.utils.data.Dataset):
         if self.add_baseline_wander:
             signal = signal + np.convolve(np.cumsum(norm.rvs(scale=0.01**(2*0.5),size=self.N)),np.hamming(self.window)/(self.window/2),mode='same')
         
-        return signal[None,].astype('float32'), masks_all
+        return signal[None,].astype('float32'), masks_all, beats
