15640839e8684abe4f4482c31692de32ccb121fe
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2020-10-08 17:47:15 +0200

    HPC

--- hpc.sh
+++ hpc.sh
@@ -4,10 +4,14 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH -o /homedtic/gjimenez/DADES/DADES/Delineator/Logs/Config_%A_%a.out
-#SBATCH -e /homedtic/gjimenez/DADES/DADES/Delineator/Logs/Config_%A_%a.err
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
+#SBATCH -o ~/DADES/DADES/Delineator/Logs/%N-%J.out
+#SBATCH -e ~/DADES/DADES/Delineator/Logs/%N-%J.err
+export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK;
 
-module load Python/3.6.4-foss-2017a
-source VirtEnv/DeepLearning/bin/activate
+module load Python/3.6.4-foss-2017a;
+source VirtEnv/DeepLearning/bin/activate;
+
+cd ~/GitHub/DelineatorSwitchAndCompose;
+
+python3 train.py --config_file ./configurations/MultiScaleUNet5Levels.json --input_files ./pickle/ --model_name Multi5All_2020_10-08;
 
