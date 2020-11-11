afd96b8688d5395deddd7c4d7627786f12838f3e
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2020-11-11 20:32:31 +0100

    sbatch update

--- hpc_F1.sh
+++ hpc_F1.sh
@@ -10,8 +10,11 @@
 #SBATCH -o /homedtic/gjimenez/DADES/DADES/Delineator/Logs/%A-%a.out
 #SBATCH -e /homedtic/gjimenez/DADES/DADES/Delineator/Logs/%A-%a.err
 
+#SLURM_ARRAY_TASK_ID=0
+
 module load Python/3.6.4-foss-2017a;
 module load PyTorch/1.6.0-foss-2017a-Python-3.6.4-CUDA-10.1.105;
+module load OpenBLAS/0.2.19-foss-2017a-LAPACK-3.7.0;
 module load OpenMPI/2.0.2-GCC-6.3.0-2.27;
 
 export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK;
