9896bbf31b1098895eec63bfe8180275d17f6457
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2020-12-15 08:55:03 +0100

    Execute local

--- hpc_test.sh
+++ hpc_test.sh
@@ -18,24 +18,33 @@ do
   list_all_models+=("$line")
 done < "$input"
 
-# Exit if array ID larger than array length
-if [ $SLURM_ARRAY_TASK_ID -gt ${#list_all_models[@]} ];
-then 
-    exit 1
-fi
+# # Exit if array ID larger than array length
+# if [ $SLURM_ARRAY_TASK_ID -gt ${#list_all_models[@]} ];
+# then 
+#     exit 1
+# fi
 
-# Get specific model
-model=${list_all_models[$SLURM_ARRAY_TASK_ID]}
+# # Get specific model
+# model=${list_all_models[$SLURM_ARRAY_TASK_ID]}
 
-module load Python/3.6.4-foss-2017a;
-module load PyTorch/1.6.0-foss-2017a-Python-3.6.4-CUDA-10.1.105;
-module load OpenBLAS/0.2.19-foss-2017a-LAPACK-3.7.0;
-module load OpenMPI/2.0.2-GCC-6.3.0-2.27;
+# module load Python/3.6.4-foss-2017a;
+# module load PyTorch/1.6.0-foss-2017a-Python-3.6.4-CUDA-10.1.105;
+# module load OpenBLAS/0.2.19-foss-2017a-LAPACK-3.7.0;
+# module load OpenMPI/2.0.2-GCC-6.3.0-2.27;
 
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK;
+# export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK;
+
+# source ~/VirtEnv/DeepLearning3/bin/activate;
+# cd ~/GitHub/DelineatorSwitchAndCompose;
+
+# python3 test.py --basedir ~/DADES/DADES/Delineator/ --model_name ${model} --hpc 1;
+
 
 source ~/VirtEnv/DeepLearning3/bin/activate;
 cd ~/GitHub/DelineatorSwitchAndCompose;
 
-python3 test.py --basedir ~/DADES/DADES/Delineator/ --model_name ${model} --hpc 1;
-
+for i in {1..108}
+do
+  model=${list_all_models[$SLURM_ARRAY_TASK_ID]}
+  python3 test.py --basedir ~/DADES/DADES/Delineator/ --model_name ${model} --hpc 0;
+done
