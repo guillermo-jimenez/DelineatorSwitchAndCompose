9f899ca7a7894c7ab2dbd563972874059369b828
Guillermo Jimenez-Perez <guillermo.jim.per@gmail.com>
2021-01-25 09:08:50 +0100

    Replaced """HPC""" script by local

---
+++ test.sh
@@ -0,0 +1,19 @@
+source ~/anaconda3/bin/activate;
+conda activate HPC;
+cd ~/GitHub/DelineatorSwitchAndCompose;
+
+# Get a list of all possible models
+list_all_models=()
+input="list_files.txt"
+while IFS= read -r line
+do
+  list_all_models+=("$line")
+done < "$input"
+
+for i in {1..108}
+do
+  model=${list_all_models[$i]}
+  python3 test_12lead.py --basedir ~/DADES/DADES/Delineator/ --model_name ${model} --hpc 0 --database ludb;
+  python3 test_12lead.py --basedir ~/DADES/DADES/Delineator/ --model_name ${model} --hpc 0 --database zhejiang;
+  python3 test_holter.py --basedir ~/DADES/DADES/Delineator/ --model_name ${model} --hpc 0;
+done
